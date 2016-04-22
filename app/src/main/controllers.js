c01109c388152ddfdd4baf613125508266511f54
aymswick <asymkowick@gmail.com>
2016-04-22 14:28:51 -0700

    Accent palette added, added cards

--- app/src/main/controllers.js
+++ app/src/main/controllers.js
@@ -13,14 +13,55 @@ app.controller('MainController', function($scope, $mdSidenav) {
 
 
 
+
   //This is how we define new functional behavior
   $scope.openLeftMenu = function openLeftMenu() {
     $mdSidenav('left').toggle(); //'left' corresponds to the component id defined in html element
   }
-
 });
 
 
-app.controller('SideNavController', function($scope) {
+app.controller('SideNavController', function($scope, $mdSidenav) {
+  //Variables
+
+
+  //Methods
+  this.goHome = function() {
+    //Home
+    $mdSidenav('left').close(); //Temporary
+    window.open(".", "_self")
+
+  }
+
+  this.goAbout = function() {
+    //About
+    window.open("https://github.com/aymswick/Quizzu-Web/wiki");
+  }
+
+  this.goSettings = function() {
+    //Settings
+  }
+
+
 
 });
+
+app.controller('CardController', function() {
+  //Variables
+  this.cardNumber = 1;
+  this.cards = [{
+    title: 'Welcome to Quizzu,'
+
+
+  }];
+
+  //Methods
+  this.setCard = function(selectedCard) {
+    this.cardNumber = selectedCard;
+  };
+
+  this.nextCard = function(cardNum) {
+    this.cardNumber = cardNum +1;
+  }
+
+})
